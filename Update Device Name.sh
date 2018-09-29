df8e27672c542364051c14e01fa5f0194fa789a4
knucklecluck <stackswell@mac.com>
2018-09-28 17:16:34 -0700

    Adding comment regarding its need to be run as root.

--- Update Device Name.sh
+++ Update Device Name.sh
@@ -3,7 +3,7 @@
 #   Script to identify "First Name" and Serial Number values,
 #   and then combine both to set as the computer's hostname
 #
-#   Fun.
+#   Needs to be run as root :)
 
 FName="$(id -P $(stat -f%Su /dev/console) | awk -F '[: ]' '{print $8}')-"
 Serial=$(system_profiler SPHardwareDataType | sed '/^ *Serial Number (system):*/!d;s###;s/ //')

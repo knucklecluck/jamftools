421e9693477ab0ad766424fbadc48910828a2a94
knucklecluck <stackswell@mac.com>
2019-05-08 17:24:39 -0700

    updating description for clarity

--- Link to LDAP.sh
+++ Link to LDAP.sh
@@ -6,11 +6,13 @@
 #       This script uses the local user's full name to guess the end user's work 
 #       email address, and then uses that value to update the computer's "user
 #       and location" data within Jamf. If the Local User's "Full Name" doesn't
-#       include a last name, this script checks the Chrome Profile instead. 
+#       include a last name, this script checks the Chrome Profile instead before
+#       assuming "firstname@company.com"
 #       
 #       This is particularly useful when using an LDAP interface within Jamf when 
 #       LDAP users are set as email addresses, and if Chrome is the standard browser
-#       for your company.
+#       for your company. Additonally, the standard email format is set as 
+#       first.last@company.com, so any deviation will need to be adjusted.
 #
 #       The Chrome component of this script is confirmed to work in macOS 10.13+
 #       and Google Chrome v70+. This component is likely very version dependent
